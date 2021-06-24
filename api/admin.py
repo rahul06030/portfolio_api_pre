cd3266427970c6bd2bc13c92c9f9cf6ff05ee352
rahul06030 <rahul06030@gmail.com>
2021-06-24 12:58:16 +0530

    final commit

--- api/admin.py
+++ api/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Education, Experience, Profile, Project
+from .models import Course, Education, Experience, Profile, Project, Skill
 
 admin.site.register(Profile)
 admin.site.register(Experience)
 admin.site.register(Education)
 admin.site.register(Project)
+admin.site.register(Course)
+admin.site.register(Skill)
